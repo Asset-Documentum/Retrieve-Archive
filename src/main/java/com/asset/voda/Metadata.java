54fad3c7e57fb49335913535872a90a2b29da6ed
MomenAlsherif <130358621+MomenAlsherif@users.noreply.github.com>
2024-12-21 23:40:19 +0200

    Initial Commit

---
+++ src/main/java/com/asset/voda/Metadata.java
@@ -0,0 +1,15 @@
+package com.asset.voda;
+
+import lombok.Data;
+
+@Data
+public class Metadata {
+    private String r_object_id;
+    private String object_name;
+    private String cch_mobile_no;
+    private String cch_sim_no;
+    private String cch_department_code;
+    private String cch_comments;
+    private String cch_sub_department_code;
+    private String r_object_type;
+}
